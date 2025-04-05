8bac94b6f67d36e92e13b5e24f47a86fc756831b
Quddus <tg53rt3@gmail.com>
2025-04-05 15:04:16 +0500

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.json({ "hello": "hi!!!!!!!"});
+});
+
+app.listen(3000);
